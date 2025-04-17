b3547449964da310b5fd75339eefa22ed91239d1
Navdeep434 <142213048+Navdeep434@users.noreply.github.com>
2025-04-17 11:00:47 +0530

    published error file to modify

---
+++ resources/views/errors/error-layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            margin: 0;
+            background: #f8fdf8;
+        }
+    </style>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
